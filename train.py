ecd242a3cff4786815362243db1b38edd5b5cc15
Rameez Ali <61817097+rameez0312@users.noreply.github.com>
2026-01-11 18:51:10 +0100

    Fix comment formatting in train.py

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 
-model = YOLO('yolov8n-pose.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8n-pose.pt')  # load a pretrained model
 
 model.train(data='config.yaml', epochs=1, imgsz=640)
